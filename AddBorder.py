63ba8f04ed9f38c5d53f10453c1064d8bd0f5689
FlachaireNathan <nathan.flachaire@outlook.fr>
2021-09-28 17:52:04 +0200

    resize percentage batch implemented

--- AddBorder.py
+++ AddBorder.py
@@ -6,14 +6,13 @@ def addBorder(image, drawable):
 
 	# get active layer
 	layer = pdb.gimp_image_get_active_layer(image)
+
 	# copy the selected layer
 	layer_copy = pdb.gimp_layer_new_from_drawable(layer, image)
+
 	# insert the layer_copy on top of the layers stack
 	layer_position = 0
 	pdb.gimp_image_insert_layer(image, layer_copy, None, layer_position)
-
-	#change active layer to copy
-	#pdb.gimp_image_set_active_layer(image, layer_copy)
 
 	#Select Non Alpha
 	pdb.gimp_selection_layer_alpha(layer_copy)
@@ -22,60 +21,43 @@ def addBorder(image, drawable):
 	radius = 3
 	pdb.gimp_selection_border(image, radius)
 
-	## COPIER COLLER
-	#pdb.gimp_image_undo_group_start(image)
-	#prev_layer = image.active_layer
-
-	# Create new layer (empty but same size)
-	#pdb.gimp_edit_copy(image.active_layer)
-	#fsel = pdb.gimp_edit_paste(drawable)
-
-	#new_layer = pdb.gimp_layer_new_from_drawable(image.active_layer, image)
-	
+	# set foreground color
+	pure_black_color = (0.0, 0.0, 0.0, 1.0)
+	foreground = pure_black_color
+	pdb.gimp_context_set_foreground(foreground)
 
 	# Fill with the border with foreground color
 	fill_type = 0 # 0: FOREGROUND 
 	pdb.gimp_drawable_edit_fill(drawable, fill_type)
-
-	#pdb.gimp_floating_sel_to_layer(fsel)
-	#image.active_layer.name = layer_name
-	#pdb.plug_in_autocrop_layer(image, image.active_layer)
-	#image.active_layer = prev_layer
-
-	#pdb.gimp_image_undo_group_end(image)
-	
 	
 	# Save image
 	new_image = pdb.gimp_image_duplicate(image)
 	layer = pdb.gimp_image_merge_visible_layers(new_image, CLIP_TO_IMAGE)
 	pdb.gimp_file_save(new_image, layer, 'D:\\Logiciel\\renpy-7.4.6-sdk\\projects\\Eternal Bonds\\game\\images\\character sets\\nivi\\test.png', '?')
-	pdb.gimp_image_delete(new_image)
-	#pdb.gimp_file_save(new_image, layer, 'D:\Logiciel\renpy-7.4.6-sdk\projects\R City\game\images\character sets\nivi\nivi.png', '?')
+	pdb.gimp_image_delete(new_image) # Delete old file after been moved
 
 	return
 
+"""
 def listAllVisible(parent, outputList):
 	for layer in parent.layers:
 		if pdb.gimp_layer_get_visible(layer):
 			outputList.append(layer)
 			if pdb.gimp_item_is_group(layer):
 				listAllVisible(layer, outputList)
+"""
 
 register(
-	"addBorder",
-	N_("Add border to image"),
-	"Add border to the image and save it to the folder specified in the script",
-	"AronGomu",
-	"@Copyright 2021",
-	"2021",
-	N_("addBorder"),
-	"",
-	[
-		#(PF_INT32, "radius", "Radius Border Pixel:", 2) # Possibility of choosing radius size
-	],
-	[],
-	addBorder,
-	menu="<Toolbox>/Scripts/",
-	domain=("gimp20-python", gimp.locale_directory))
+    "python_fu_test_hello_world",
+    "Hello world",
+    "Display a 'hello world' message",
+    "JFM",
+    "Open source (BSD 3-clause license)",
+    "2013",
+    "<Image>/Filters/AddBorder",
+    "*",
+    [],
+    [],
+    addBorder)
 
 main()
--- resizePercentageBatch.py
+++ resizePercentageBatch.py
@@ -6,7 +6,7 @@ import os
 
 pdb = gimp.pdb
 
-def addBorderToBatch(loadfolder):
+def resizePercentageBatch(scale, loadfolder):
 
 # correct loadfolder: add (back-)slash if needed
     if not loadfolder.endswith(("/", "\\")):
@@ -31,31 +31,26 @@ def addBorderToBatch(loadfolder):
             outputfolder = "%soutput\\" % loadfolder # add the name of a new folder
         else:
             outputfolder = "%soutput//" % loadfolder # add the name of a new folder
-        gimp.message(outputfolder)
+        #gimp.message(outputfolder)
         if not os.path.exists(outputfolder):
             os.makedirs(outputfolder) # create the new folder if it doesn't exist yet
         filename = os.path.basename(filepath) # remove the path and only keep the actual filename with extension
         outputpath = outputfolder + filename
 
-        # now the actual work
+        # Get image width and height
+        width_image = pdb.gimp_image_width(image)
+        height_image = pdb.gimp_image_height(image)
 
-        # copy the selected layer
-        layer_copy = pdb.gimp_layer_new_from_drawable(layer, image)
-
-        # insert the layer_copy on top of the layers stack
-        layer_position = 0
-        pdb.gimp_image_insert_layer(image, layer_copy, None, layer_position)
-
-        #Select Non Alpha
-        pdb.gimp_selection_layer_alpha(layer_copy)
-
-        # Select border with specified radius
-        radius = 3
-        pdb.gimp_selection_border(image, radius)
-
-        # Fill with the border with foreground color
-        fill_type = 0 # 0: FOREGROUND 
-        pdb.gimp_drawable_edit_fill(layer, fill_type)
+        # Crop the 4 first lines because of koikatsu studio artefacts
+        new_width = int(width_image * scale)
+        new_height = int(height_image * scale)
+        #offx = 0
+        #offy = 0
+        #pdb.gimp_image_resize(image, new_width, new_height, offx, offy)
+        pdb.gimp_image_scale(image, new_width, new_height)
+        # Crop to content
+        drawable = layer
+        pdb.plug_in_autocrop(image, drawable)
 
         # Merge layers to export the image as png
         image.merge_visible_layers(0)
@@ -67,20 +62,21 @@ def addBorderToBatch(loadfolder):
 
 
 register(
-    "addBorderToBatch",
-	N_("Add border to a batch of images."),
-	"Add border to all .png file in specified folder and save them in a output folder in the same folder.",
+    "resizePercentageBatch",
+	N_("Resize a batch of image with a given percentage"),
+	"Resize a batch of image with a given percentage",
 	"AronGomu",
 	"@Copyright 2021",
 	"2021",
-	N_("addBorderToBatch"),
+	N_("resizePercentageBatch"),
 	"",
 	[
+    (PF_FLOAT, "scale", "The scaling of resizing, number to multiply to the height and width.", ""),
     (PF_STRING, "loadfolder", "The location of your images.", "")
 	],
 	[],
-	addBorderToBatch,
-	menu="<Toolbox>/Scripts/",
+	resizePercentageBatch,
+	menu="<Toolbox>/Filters/",
 	domain=("gimp20-python", gimp.locale_directory)
 )
 
